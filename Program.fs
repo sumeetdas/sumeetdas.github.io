5094b3ca478064f0d11c9e38bb59a5b1b0ca2e42
Sumeet Das <sumeetdas1992@gmail.com>
2021-12-24 21:37:16 +0530

    converted it into f# project

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿open Feliz.ViewEngine
+
+let html =
+    Html.h1 [
+        prop.style [ style.fontSize(100); style.color("#137373") ]
+        prop.text "Hello, world!"
+    ]
+    |> Render.htmlView
+
+printfn "Output: %s" html
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
