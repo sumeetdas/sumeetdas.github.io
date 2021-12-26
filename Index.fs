e9fe767b60f89114e83e60014ef01bf9b67c4f31
Sumeet Das <sumeetdas1992@gmail.com>
2021-12-26 20:00:34 +0530

    checkpoint

--- Index.fs
+++ Index.fs
@@ -3,52 +3,78 @@ module Index
 open _Common
 open Feliz.ViewEngine
 
-let private paraElem (text: string) = 
-    Html.p [ 
-        prop.classes [
-            "dark:text-gray-200"
-            "text-2xl"
-            "my-3"
-            "leading-8"
-        ]
-        prop.text text 
+let private paraClasses = 
+    [
+        "dark:text-gray-200"
+        "lg:text-2xl"
+        "my-3"
+        "leading-8"
+        "px-6"
     ]
 
-let private toPara (parts: string list) = 
-    parts |> String.concat " " |> paraElem
+let private toParaWithStr (parts: string list) = 
+    Html.p [
+        prop.classes paraClasses
+        prop.text (parts |> String.concat " ")
+    ]
+
+let private toParaWithElems (elems: ReactElement list) = 
+    Html.p [
+        prop.classes paraClasses
+        prop.children elems
+    ]
+
+let private getLink (href: string, name: string) = 
+    Html.a [
+        prop.classes [
+            "border-b-2"
+        ]
+        prop.target "_blank"
+        prop.text name
+        prop.href href
+    ]
+
+let private toText (parts: string list) = 
+    Html.text (parts |> String.concat " ")
 
 let private aboutMe = 
     [
         [
-            "I'm a software developer currently writing Java code for a living."
-            "I love functional programming and writing code in F#."
-            "My current interests are - F# / Rust / Distributed Systems / Developing"
-            "Mobile and Web apps."
-        ] |> toPara
+            [
+                "I'm a software developer who writes Java code for a living."
+                "I love functional programming and writing code in F#."
+                "I have even developed this website "
+            ] |> toText
+            getLink ("https://github.com/sumeetdas/sumeetdas.github.io", "using F#")
+            [
+                ". My current interests are - F# / Rust / Distributed Systems / Developing"
+                "Mobile and Web apps."
+            ] |> toText
+        ] |> toParaWithElems
         [
-            "I currently live in India and I'm looking for"
+            "I live in India and I'm looking for"
             "fully remote jobs at the moment. I'm also open to"
             "relocating to other countries if need be."
-        ] |> toPara
+        ] |> toParaWithStr
         [
             "You can follow me on GitHub and Twitter, or if you"
             "want to contact me, you can do so via Twitter DMs"
             "(Direct Message). I'm also available on LinkedIn"
-            "but since I find the site quite depressing (more"
-            "Insta-level posts and corporate cringe),"
+            "but since I find the site quite depressing (nowadays more"
+            "Insta-level and corporate cringe posts),"
             "I'm not very active there."
-        ] |> toPara
+        ] |> toParaWithStr
     ]
 
 let private links = 
     let list = [
-        "GitHub", "icons/github.svg", "https://github.com/sumeetdas"
-        "Twitter", "icons/twitter.svg", "https://twitter.com/_sumeetdas"
-        "LinkedIn", "icons/linkedin.svg", "https://in.linkedin.com/in/das-sumeet"
+        "icons/github.svg", "https://github.com/sumeetdas"
+        "icons/twitter.svg", "https://twitter.com/_sumeetdas"
+        "icons/linkedin.svg", "https://in.linkedin.com/in/das-sumeet"
     ]
 
-    let elem (item: string * string * string) = 
-        let name, icon, link = item
+    let elem (item: string * string) = 
+        let icon, link = item
         Html.a [
             prop.classes [
                 "inline-flex"
@@ -87,41 +113,34 @@ let private links =
     ]
 
 let html =
-    Html.div [
-        prop.classes [
-            tw.``flex``
-            tw.``flex-col``
-            "my-auto"
+    [
+        Html.img [
+            prop.classes [ 
+                tw.``flex``
+                tw.``flex-col``
+                tw.``w-48``
+                tw.``h-auto``
+                "mx-auto"
+            ]
+            prop.src "myphoto.jpg" 
         ]
-        prop.children [
-            Html.img [
-                prop.classes [ 
-                    tw.``flex``
-                    tw.``flex-col``
-                    tw.``w-48``
-                    tw.``h-auto``
-                    "mx-auto"
-                ]
-                prop.src "myphoto.jpg" 
+        Html.h1 [
+            prop.classes [
+                "text-black"
+                "dark:text-slate-200"
+                "text-5xl"
+                "text-center"
+                "my-3"
             ]
-            Html.h1 [
-                prop.classes [
-                    "text-black"
-                    "dark:text-slate-200"
-                    "text-4xl"
-                    "text-center"
-                    "my-3"
-                ]
-                prop.text "Sumeet Das"
+            prop.text "Sumeet Das"
+        ]
+        Html.div [
+            prop.classes [
+                "flex"
+                "flex-col"
+                "my-auto"
             ]
-            Html.div [
-                prop.classes [
-                    "flex"
-                    "flex-col"
-                    "my-auto"
-                ]
-                prop.children (aboutMe @ [ links ])
-            ]
+            prop.children (aboutMe @ [ links ])
         ]
     ]
 
