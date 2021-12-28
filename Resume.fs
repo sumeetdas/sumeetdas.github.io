057e6af5064b517764d93415fe4c1bc073d9bd16
Sumeet Das <sumeetdas1992@gmail.com>
2021-12-28 09:10:41 +0530

    checkpoint

--- Resume.fs
+++ Resume.fs
@@ -2,6 +2,16 @@ module Resume
 
 open Feliz.ViewEngine
 
+let anchor (name: string, href: string) = 
+    Html.a [
+        prop.classes [
+            "border-b"
+        ]
+        prop.target "_blank"
+        prop.text name
+        prop.href href
+    ]
+
 let intro = 
     Html.div [
         prop.classes [
@@ -29,10 +39,24 @@ type Experience =
         Highlights: ReactElement list
     }
 
+let unorderedList (listItems: ReactElement list) = 
+    Html.ul [
+        prop.classes [
+            "list-disc"
+            "list-inside"
+        ]
+        prop.children listItems
+    ]
+
 let experienceList: Experience list = 
     let listElem (text: string) = 
         Html.li [
             prop.text text
+        ]
+    
+    let listElemHtml (children: ReactElement list) = 
+        Html.li [
+            prop.children children
         ]
 
     let concat (parts: string list) = parts |> String.concat " "
@@ -42,20 +66,177 @@ let experienceList: Experience list =
             Title = "sumeetdas.me"
             Duration = "24-Dec-2021 - 27-Dec-2021"
             Summary = "Rebuilt my website using F#"
-            Highlights = [
-                Html.ul [
-                    prop.classes [
-                        "list-disc"
-                        "list-inside"
+            Highlights = 
+                [
+                    [
+                        "Rebuilt my personal website as a static"
+                        "website using F#, Tailwind CSS and Vanilla JS"
+                    ] |> concat |> listElem
+                    Html.li [
+                        prop.children [
+                            Html.text "URL - "
+                            anchor ("https://sumeetdas.me", "https://sumeetdas.me")
+                        ]
                     ]
-                    prop.children (
+                ] |> unorderedList |> fun elem -> [ elem ]
+        }
+        {
+            Title = "Oracle India Pvt. Ltd."
+            Duration = "April-2018 - Present"
+            Summary = "Working on ClinicalOne product"
+            Highlights = 
+                [
+                    [
+                        "Rebuilt my personal website as a static"
+                        "website using F#, Tailwind CSS and Vanilla JS"
+                    ] |> concat |> listElem
+                    [
+                        "Involved in improving translatability of"
+                        "the application"
+                    ] |> concat |> listElem
+                    [
+                        Html.text "Product URL - "
+                        anchor ("https://www.oracle.com/industries/life-sciences/clinical-one/", "https://www.oracle.com/industries/life-sciences/clinical-one/")
+                    ] |> listElemHtml
+                ] |> unorderedList |> fun elem -> [ elem ]
+        }
+        {
+            Title = "Oracle India Pvt. Ltd."
+            Duration = "July-2013 - March-2018"
+            Summary = "Worked on ClearTrial product"
+            Highlights = 
+                [
+                    [
+                        "Rebuilt my personal website as a static"
+                        "website using F#, Tailwind CSS and Vanilla JS"
+                    ] |> concat |> listElem
+                    Html.li [
+                        prop.children [
+                            Html.text "Product URL - "
+                            anchor (
+                                "https://www.oracle.com/in/industries/life-sciences/cleartrial-portfolio-planning.html", 
+                                "https://www.oracle.com/in/industries/life-sciences/cleartrial-portfolio-planning.html"
+                            )
+                        ]
+                    ]
+                ] |> unorderedList |> fun elem -> [ elem ]
+        }
+        {
+            Title = "Meow Blogging Engine"
+            Duration = "2015"
+            Summary = "Personal project on building blogging engine"
+            Highlights = 
+                [
+                    [
+                        "Rebuilt my personal website as a static"
+                        "website using F#, Tailwind CSS and Vanilla JS"
+                    ] |> concat |> listElem
+                    [
+                        "Project is now archived. Links: " |> listElem
                         [
-                            "Rebuilt my personal website as a static"
-                            "website using F#, Tailwind CSS and Vanilla JS"
-                        ] |> concat |> listElem
-                    )
-                ]
-            ]
+                            "Meow: The core blogging engine" |> listElem
+                            [
+                                [
+                                    Html.text "Link: "
+                                    anchor (
+                                        "https://github.com/sumeetdas/Meow", 
+                                        "https://github.com/sumeetdas/Meow"
+                                    )
+                                ] |> listElemHtml
+                            ] |> unorderedList
+                        ] |> unorderedList
+                    ] |> unorderedList
+                ] |> unorderedList |> fun elem -> [ elem ]
+        }
+        {
+            Title = "sumeetdas.me"
+            Duration = "24-Dec-2021 - 27-Dec-2021"
+            Summary = "Rebuilt my website using F#"
+            Highlights = 
+                [
+                    [
+                        "Rebuilt my personal website as a static"
+                        "website using F#, Tailwind CSS and Vanilla JS"
+                    ] |> concat |> listElem
+                    Html.li [
+                        prop.children [
+                            Html.text "URL - "
+                            anchor ("https://sumeetdas.me", "https://sumeetdas.me")
+                        ]
+                    ]
+                ] |> unorderedList |> fun elem -> [ elem ]
+        }
+        {
+            Title = "sumeetdas.me"
+            Duration = "24-Dec-2021 - 27-Dec-2021"
+            Summary = "Rebuilt my website using F#"
+            Highlights = 
+                [
+                    [
+                        "Rebuilt my personal website as a static"
+                        "website using F#, Tailwind CSS and Vanilla JS"
+                    ] |> concat |> listElem
+                    Html.li [
+                        prop.children [
+                            Html.text "URL - "
+                            anchor ("https://sumeetdas.me", "https://sumeetdas.me")
+                        ]
+                    ]
+                ] |> unorderedList |> fun elem -> [ elem ]
+        }
+        {
+            Title = "sumeetdas.me"
+            Duration = "24-Dec-2021 - 27-Dec-2021"
+            Summary = "Rebuilt my website using F#"
+            Highlights = 
+                [
+                    [
+                        "Rebuilt my personal website as a static"
+                        "website using F#, Tailwind CSS and Vanilla JS"
+                    ] |> concat |> listElem
+                    Html.li [
+                        prop.children [
+                            Html.text "URL - "
+                            anchor ("https://sumeetdas.me", "https://sumeetdas.me")
+                        ]
+                    ]
+                ] |> unorderedList |> fun elem -> [ elem ]
+        }
+        {
+            Title = "sumeetdas.me"
+            Duration = "24-Dec-2021 - 27-Dec-2021"
+            Summary = "Rebuilt my website using F#"
+            Highlights = 
+                [
+                    [
+                        "Rebuilt my personal website as a static"
+                        "website using F#, Tailwind CSS and Vanilla JS"
+                    ] |> concat |> listElem
+                    Html.li [
+                        prop.children [
+                            Html.text "URL - "
+                            anchor ("https://sumeetdas.me", "https://sumeetdas.me")
+                        ]
+                    ]
+                ] |> unorderedList |> fun elem -> [ elem ]
+        }
+        {
+            Title = "sumeetdas.me"
+            Duration = "24-Dec-2021 - 27-Dec-2021"
+            Summary = "Rebuilt my website using F#"
+            Highlights = 
+                [
+                    [
+                        "Rebuilt my personal website as a static"
+                        "website using F#, Tailwind CSS and Vanilla JS"
+                    ] |> concat |> listElem
+                    Html.li [
+                        prop.children [
+                            Html.text "URL - "
+                            anchor ("https://sumeetdas.me", "https://sumeetdas.me")
+                        ]
+                    ]
+                ] |> unorderedList |> fun elem -> [ elem ]
         }
     ]
 
@@ -105,6 +286,75 @@ let programmingExperience =
         ] @ (experienceList |> List.map experienceElem))
     ]
 
+type Education = 
+    {
+        Institution: string
+        Course: string
+        Duration: string
+        Highlights: ReactElement list
+    }
+
+let educationList: Education list = 
+    let listItem (text: string) = 
+        Html.li [
+            prop.classes [ 
+                "my-1"
+            ]
+            prop.text text
+        ]
+
+    [
+        {
+            Institution = "BITS Pilani - Pilani Campus"
+            Course = "B.E. (Hons.) Electronics and Instrumentation"
+            Duration = "Aug 2009 - June 2013"
+            Highlights = [
+                listItem "Graduated with First Class Honors and a GPA of 7.61"
+                [
+                    "Electives completed: Fuzzy Logic, Image Processing,"
+                    "Effective Public Speaking, Object Oriented"
+                    "Programming, International Relations"
+                ] |> String.concat " " |> listItem
+            ]
+        }
+    ]
+
+let educationElem (education: Education) = 
+    Html.div [
+        prop.children [
+            Html.div [
+                prop.text education.Institution
+            ]
+            Html.div [
+                prop.text education.Course
+            ]
+            Html.div [
+                prop.text education.Duration
+            ]
+            Html.div [
+                education.Highlights |> unorderedList
+            ]
+        ]
+    ]
+
+let education =
+    Html.div [
+        prop.classes [
+            "text-gray-200"
+        ]
+        prop.children ([
+            Html.h2 [
+                prop.classes [
+                    "text-3xl"
+                    "text-bold"
+                    "border-b-2"
+                    "pb-2"
+                ]
+                prop.text "Education"
+            ]
+        ] @ (educationList |> List.map educationElem))
+    ]
+
 let links = 
     let list: (string * string) list = 
         [
@@ -117,19 +367,14 @@ let links =
     let linkElem (link: string * string) = 
         let name, href = link
         Html.li [
-            prop.classes []
+            prop.classes [
+                "my-1"
+            ]
             prop.children [
                 Html.span [
                     prop.text (sprintf "%s - " name)
                 ]
-                Html.a [
-                    prop.classes [
-                        "border-b"
-                    ]
-                    prop.target "_blank"
-                    prop.href href
-                    prop.text href
-                ]
+                anchor (href, href)
             ]
         ]
 
@@ -186,6 +431,7 @@ let resume =
         prop.children [
             intro
             programmingExperience
+            education
             links
         ]
     ]
