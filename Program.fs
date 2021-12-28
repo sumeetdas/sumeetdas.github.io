32ad84a50b4c8d9f99a02ab720a63acf0d6af4ee
Sumeet Das <sumeetdas1992@gmail.com>
2021-12-28 19:06:51 +0530

    renamed public as docs

--- Program.fs
+++ Program.fs
@@ -1,5 +1,5 @@
 ﻿open _Common
 open _Layout
 
-Index.html |> htmlToString Page.Index |> generateFile "public/index.html"
-Resume.html |> htmlToString Page.Resume |> generateFile "public/resume.html"
+Index.html |> htmlToString Page.Index |> generateFile "docs/index.html"
+Resume.html |> htmlToString Page.Resume |> generateFile "docs/resume.html"
--- _Common.fs
+++ _Common.fs
@@ -3,7 +3,7 @@ module _Common
 open Zanaptak.TypedCssClasses
 open System.IO
 
-type tw = CssClasses<"public/style.css">
+type tw = CssClasses<"docs/style.css">
 
 let generateFile (fileName: string) (html: string) =
     File.WriteAllBytes (fileName, html |> System.Text.Encoding.ASCII.GetBytes)
