1fd39700eecdf8031971d3aa04fb6cf1df3edeca
Sumeet Das <sumeetdas1992@gmail.com>
2021-12-28 18:59:31 +0530

    fixed issues

--- _Common.fs
+++ _Common.fs
@@ -3,7 +3,7 @@ module _Common
 open Zanaptak.TypedCssClasses
 open System.IO
 
-type tw = CssClasses<"public/output.css">
+type tw = CssClasses<"public/style.css">
 
 let generateFile (fileName: string) (html: string) =
     File.WriteAllBytes (fileName, html |> System.Text.Encoding.ASCII.GetBytes)
